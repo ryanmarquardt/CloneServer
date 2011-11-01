5fd70a0a024cba1ae41206bd089d18b13f092d4c
Ryan Marquardt <ryan.marquardt@gmail.com>
2011-11-01 12:30:27 -0700

    Fixed memory leak in hashsplit

--- cloneserver/hashsplitmodule.c
+++ cloneserver/hashsplitmodule.c
@@ -18,8 +18,8 @@ typedef struct {
 Chunk * chunk_new(int size) {
 	return (Chunk *)malloc(sizeof(Chunk) + size*sizeof(uint8_t));
 }
-void    chunk_delete(Chunk *self) { return free(self); }
-uint    chunk_digest(Chunk *self) { return (self->s1 << 16) | self->s2; }
+void chunk_delete(Chunk *self) { return free(self); }
+inline uint chunk_digest(Chunk *self) { return (self->s1 << 16) | self->s2; }
 
 void chunk_init(Chunk *self, int offset) {
 	self->offset = offset;
@@ -28,24 +28,28 @@ void chunk_init(Chunk *self, int offset) {
 	self->length = 0;
 }
 
-int chunk_append(Chunk *self, unsigned char add, long MAX_CHUNK_SIZE) {
-	int cursor = self->length - MIN_CHUNK_SIZE;
+void chunk_append(Chunk *self, unsigned char add) {
+	const int cursor = self->length - MIN_CHUNK_SIZE;
 	//Don't bother zeroing memory...
 	uint8_t drop = (cursor >= 0) ? self->data[cursor] : 0 ;
 	self->data[self->length++] = add;
 	self->s1 += add - drop;
 	self->s2 += self->s1 - MIN_CHUNK_SIZE * (drop + CHAR_OFFSET);
-	return (self->length < MAX_CHUNK_SIZE);
-	//Returns true if we can add more to this chunk
 }
 
 unsigned read_chunk(FILE *source, Chunk *chunk, long MAX_CHUNK_SIZE) {
 	int buf;
 	chunk_init(chunk, ftell(source));
+	while (chunk->length < MIN_CHUNK_SIZE) {
+		if ((buf = fgetc(source))==EOF)
+			return (chunk->length > 0);
+		chunk_append(chunk, (char)buf);
+	}
 	while ((~(chunk_digest(chunk)) & MATCHMASK) || (chunk->length < MIN_CHUNK_SIZE)) {
 		if ((buf = fgetc(source))==EOF)
 			return (chunk->length > 0); //No more if this chunk has nothing
-		if (!(chunk_append(chunk, (char)(buf & 0xff), MAX_CHUNK_SIZE)))
+		chunk_append(chunk, (char)buf);
+		if (chunk->length >= MAX_CHUNK_SIZE)
 			return 1; //Chunk has data, but it's too big to take more
 	}
 	return 1;
@@ -55,27 +59,32 @@ static PyObject * hashsplit_read_chunk(PyObject *self, PyObject *args) {
 	PyObject *source_file = NULL; //gcc complains if these aren't initialized
 	PyObject *max_chunk_size = NULL;
 	long MAX_CHUNK_SIZE;
-	PyObject *result;
+	PyObject *result = NULL;
 	Chunk *chunk;
 	FILE *source;
 
-	if (!PyArg_UnpackTuple(args, "read_chunk", 1, 2, &source_file, &max_chunk_size)) return NULL;
+	if (!PyArg_UnpackTuple(args, "read_chunk", 1, 2, &source_file, &max_chunk_size))
+		return NULL;
 	if (!(source = PyFile_AsFile(source_file))) {
-		Raise(PyExc_TypeError, "Expected file or file descriptor");
+		PyErr_SetString(PyExc_TypeError, "Expected file or file descriptor");
+		return NULL;
 	}
 	if (!max_chunk_size || max_chunk_size == Py_None) {
 		MAX_CHUNK_SIZE = DEFAULT_MAX_CHUNK_SIZE;
 	} else if ((MAX_CHUNK_SIZE = PyInt_AsLong(max_chunk_size))==-1 && PyErr_Occurred()) {
-		Raise(PyExc_TypeError, "max_chunk_size should be an integer");
+		PyErr_SetString(PyExc_TypeError, "max_chunk_size should be an integer");
+		return NULL;
 	}
 	if (MAX_CHUNK_SIZE < MIN_CHUNK_SIZE) {
-		Raise(PyExc_ValueError, "max_chunk_size must be larger than MIN_CHUNK_SIZE"); }
+		PyErr_SetString(PyExc_ValueError, "max_chunk_size must be larger than MIN_CHUNK_SIZE");
+		return NULL;
+	}
 	if (!(chunk = chunk_new(MAX_CHUNK_SIZE))) return PyErr_NoMemory();
-	if (!read_chunk(source, chunk, MAX_CHUNK_SIZE)) { Raise(PyExc_EOFError, ""); }
-	result = PyTuple_Pack(3,
-		PyLong_FromUnsignedLong(chunk->offset),
-		PyInt_FromLong(chunk_digest(chunk)),
-		PyString_FromStringAndSize((char*)chunk->data, chunk->length));
+	if (!read_chunk(source, chunk, MAX_CHUNK_SIZE)) {
+		PyErr_SetString(PyExc_EOFError, "");
+	} else {
+		result = PyString_FromStringAndSize((char*)chunk->data, chunk->length);
+	}
 	chunk_delete(chunk);
 	return result;
 }
@@ -91,4 +100,5 @@ PyMODINIT_FUNC init_hashsplit(void) {
 	PyObject *mod;
 	mod = Py_InitModule3("_hashsplit", hashsplit_methods, "Split a file into chunks ala rsync");
 	PyModule_AddObject(mod, "MIN_CHUNK_SIZE", PyInt_FromLong(MIN_CHUNK_SIZE));
+	PyModule_AddObject(mod, "DEFAULT_MAX_CHUNK_SIZE", PyInt_FromLong(DEFAULT_MAX_CHUNK_SIZE));
 }
